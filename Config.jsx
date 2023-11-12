c0c30496fa30da6ba1f63e2095bc82bc246d4175
siffahim <siffahim25@gmail.com>
2023-11-12 18:42:06 +0600

    opti:

--- Config.jsx
+++ Config.jsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const baseAxios = axios.create({
   baseURL: "http://167.99.205.107:5000",
-  // baseURL: "http://167.99.205.107:5000",
-  timeout: 10000,
+  timeout: 50000,
   headers: { "X-Custom-Header": "foobar" },
 });
 
